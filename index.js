65839ea8a8f1f0de379d2524a57d944d945581a4
ray <ray@codeinstitute.net>
2021-03-02 16:49:42 +0000

    Parking the main code in the cred file write callback

--- index.js
+++ index.js
@@ -14,42 +14,46 @@ const io = require('socket.io')(server);
 io.on('connection', (socket) => {
     console.log("Socket Connected"); 
     console.log("CREDS: ", process.env.CREDS, 'utf8', function(err) {
-        console.log('Error writing file: ', err);
+        if (err) {
+            console.log('Error writing file: ', err);
+            socket.emit("console_output", "Error saving credentials: " + err);
+        } else {
+
+            fs.writeFile('creds.json', process.env.CREDS);
+            let pyshell = new PythonShell('run.py');
+
+
+            socket.on('disconnect', () =>  {
+                console.log("Socket Disconnected")
+                pyshell.end();
+            });
+
+            socket.on('command_entered', (command) =>  {
+                console.log("Socket Command: ", command)
+                pyshell.send(command);
+            });
+
+
+            // sends a message to the Python script via stdin
+
+            pyshell.on('message',  (message) => {
+                // received a message sent from the Python script (a simple "print" statement)
+                console.log('process Out: ', message);
+                socket.emit("console_output", "Out: " + message);
+            });
+
+            pyshell.on('close', () => {
+                console.log('Process ended');
+                socket.emit("console_output", "Process ended: ");
+            });
+
+            pyshell.on('error', (message) => {
+                console.log('Process error:', message);
+                socket.emit("console_output", "Process error: " + message);
+            });
+
+        }
     });
-
-    fs.writeFile('creds.json', process.env.CREDS);
-    let pyshell = new PythonShell('run.py');
-
-
-    socket.on('disconnect', () =>  {
-        console.log("Socket Disconnected")
-        pyshell.end();
-    });
-
-    socket.on('command_entered', (command) =>  {
-        console.log("Socket Command: ", command)
-        pyshell.send(command);
-    });
-
-
-    // sends a message to the Python script via stdin
-
-    pyshell.on('message',  (message) => {
-        // received a message sent from the Python script (a simple "print" statement)
-        console.log('process Out: ', message);
-        socket.emit("console_output", "Out: " + message);
-    });
-
-    pyshell.on('close', () => {
-        console.log('Process ended');
-        socket.emit("console_output", "Process ended: ");
-    });
-
-    pyshell.on('error', (message) => {
-        console.log('Process error:', message);
-        socket.emit("console_output", "Process error: " + message);
-    });
-
 });
 
 
